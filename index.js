bfbdae2198ff0b482e6124932ccb3608ff12bcf6
huzaifanur <huzaifa98nur@gmail.com>
2023-04-22 05:12:21 +0500

    multipgaepdf

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { createPdf } = require("./createPdf");
+const { createMultipagePdf } = require("./testFunctions");
 const app = express();
 app.use(express.json());
 
@@ -8,42 +9,8 @@ const PORT = process.env.PORT || 4000;
 app.post("/pdf", (req, res) => {
   createPdf(req.body, res);
 });
-app.get("/pdf", (req, res) => {
-  const productName = "Chicken";
-  const ingredients = `
-  1. Chicken breast
-  2. Salt
-  3. Pepper
-  4. Onion
-  5. Garlic
-  6. Oil
-  7. Pepperoni
-  7. Chillies
-  8. Tomato
-  8. Tomatoes
-  9. Basil
-  9. Onion
-  `;
-  const instructions = `
-  1. Heat oil in a large pan
-  2. Add chicken
-  3. Add onion
-  4. Add garlic
-  5. Add salt
-  6. Add pepper
-  7. Add pepperoni
-  8. Add chillies
-  9. Add tomato
-  `;
-  const productImg =
-    "https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w";
-  const body = {
-    productName,
-    ingredients,
-    instructions,
-    productImg,
-  };
-  createPdf(body, res);
+app.post("/muti-pdf", (req, res) => {
+  createMultipagePdf(req.body, res);
 });
 
 app.get("/", (req, res) => {
---
+++ testFunctions.js
@@ -0,0 +1,169 @@
+const puppeteer = require("puppeteer");
+require("dotenv").config();
+const PDFMerger = require("pdf-merger-js");
+
+async function createMultipagePdf(bodyArr, res) {
+  bodyArr.push("padding");
+  const browser = await puppeteer.launch({
+    args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",
+    ],
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
+  });
+  var merger = new PDFMerger();
+  try {
+    for (let i = 0; i < bodyArr.length; i++) {
+      const body = bodyArr[i];
+      const html = getFilledTemplate(body);
+      const page = await browser.newPage();
+      await page.setContent(html);
+      await page.emulateMediaType("screen");
+      await page.setViewport({
+        width: 1200,
+        height: 800,
+        deviceScaleFactor: 2,
+      });
+      merger.add(await page.pdf({ printBackground: true }));
+    }
+    const mergedPdfBuffer = await merger.saveAsBuffer();
+    res.type("pdf");
+    res.send(mergedPdfBuffer);
+  } catch (e) {
+    console.error(e);
+    res.status(500).send("Something went wrong");
+  } finally {
+    await browser.close();
+  }
+}
+
+function getFilledTemplate(body) {
+  const { productName, ingredients, instructions, productImg } = body;
+  const html = `<!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <style>
+    
+        *,
+        *::before,
+        *::after {
+          box-sizing: border-box;
+          padding: 0;
+          margin: 0;
+        }
+        body {
+          font-family: "Poppins", sans-serif;
+        }
+        ul,
+        ol {
+          list-style: none;
+        }
+        img {
+          max-width: 100%;
+          display: block;
+        }
+        img {
+          object-fit: cover;
+          height: 400px;
+        }
+        h2 {
+          margin-bottom: 16px;
+        }
+        p {
+          font-size: 1.2rem;
+          line-height: 1.5;
+        }
+        .container {
+          margin-inline: auto;
+          display: flex;
+          height: 100vh;
+        }
+        .col > p {
+          font-size: 1.2rem;
+        }
+        .col-1 {
+          flex-basis: 50%;
+          color: white;
+          background-color: #264472;
+          padding: 2rem;
+          height:1056px;
+          
+        }
+        .col-2 {
+          flex-basis: 50%;
+        }
+    
+        .ing-p {
+          margin-bottom: 50px;
+        }
+    
+        .underline {
+          border-bottom: 2px solid rgb(184, 184, 184);
+          padding-bottom: 3px;
+          width: fit-content;
+          letter-spacing: 3px;
+        }
+    
+        .meal {
+          padding: 2rem;
+        }
+        .meal-heading {
+          text-align: center;
+          line-height: 1.2;
+          margin: 2rem 0 1rem;
+        }
+    
+        .meal-content {
+        }
+      </style>
+      <body>
+        <div class="container">
+          <div class="col col-1">
+            <h2 class="underline">INGREDIENTS</h2>
+            <p class="ing-p">
+              ${ingredients} 
+            </p>
+    
+            <h2 class="underline">INSTRUCTIONS</h2>
+            <p>
+              ${instructions}
+            </p>
+          </div>
+          <div class="col col-2">
+            <img
+              class=".image"
+              src=${productImg}
+              alt=""
+            />
+            <div class="meal">
+              <h2 class="meal-heading">$${productName}</h2>
+              <p class="meal-content">
+                15 minutes Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Nulla fringilla lectus eget nibh accumsan scelerisque. Sed nec metus
+                et tellus dignissim egestas quis vel erat. Vestibulum vel facilisis
+                ex. Sed semper neque nec hendrerit vestibulum. Etiam ut turpis nec
+                nibh consectetur ullamcorper.
+              </p>
+            </div>
+          </div>
+        </div>
+      </body>
+    </html>
+    `;
+  return html;
+}
+
+module.exports = { createMultipagePdf };
