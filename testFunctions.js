d3ed10f1a572cfe24b4b0b2c77a972448f808cd3
huzaifanur <huzaifa98nur@gmail.com>
2023-04-22 23:59:08 +0500

    removed image

--- testFunctions.js
+++ testFunctions.js
@@ -48,7 +48,7 @@ async function createMultipagePdf(dataArr, res) {
 
 function getFilledTemplate(body) {
   const { productName, ingredients, instructions } = body;
-  let productImg = "https://picsum.photos/100";
+  let productImg = "";
 
   const html = `<!DOCTYPE html>
     <html lang="en">
